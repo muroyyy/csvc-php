d2fbd00af7513a73220e2d75b9f9bc0925a4186f
muroyyy <amirul.faiz01@gmail.com>
2025-08-15 17:29:33 +0800

    indentation changes

--- query.php
+++ query.php
@@ -1,43 +1,42 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>Query Page</title>
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body class="bodyStyle">
+	<head>
+		<meta charset="UTF-8">
+		<title>Query Page</title>
+		<link rel="stylesheet" href="css/styles.css">
+	</head>
+	<body class="bodyStyle">
 
-    <div id="header" class="mainHeader">
-        <hr>
-        <div class="center">
-            <h1>Example Social Research Organization</h1>
-        </div>
-        <hr>
-        <div class="center">
-            <h2>Country Data Query Page</h2>
-        </div>
-    </div>
+		<div id="header" class="mainHeader">
+			<hr>
+			<div class="center">
+				<h1>Example Social Research Organization</h1>
+			</div>
+			<hr>
+			<div class="center">
+				<h2>Country Data Query Page</h2>
+			</div>
+		</div>
 
-    <br>
-    <h2><a href="index.php">Home</a></h2>
-    <h2>Please select which query you want to run.</h2>
+		<br>
+		<h2><a href="index.php">Home</a></h2>
+		<h2>Please select which query you want to run.</h2>
 
-    <form id="form1" method="post" action="query2.php">
-        <select name="selection">
-            <option value="">Select...</option>
-            <option value="Q1">Mobile phones</option>
-            <option value="Q2">Population</option>
-            <option value="Q3">Life Expectancy</option>
-            <option value="Q4">GDP</option>
-            <option value="Q5">Childhood Mortality</option>
-        </select>
-        <input type="submit" value="Submit">
-    </form>
+		<form id="form1" method="post" action="query2.php">
+			<select name="selection">
+				<option value="">Select...</option>
+				<option value="Q1">Mobile phones</option>
+				<option value="Q2">Population</option>
+				<option value="Q3">Life Expectancy</option>
+				<option value="Q4">GDP</option>
+				<option value="Q5">Childhood Mortality</option>
+			</select>
+			<input type="submit" value="Submit">
+		</form>
 
-    <br>
-    <div id="Copyright" class="center">
-        <h5>&copy; 2020, Amazon Web Services, Inc. or its Affiliates. All rights reserved.</h5>
-    </div>
-
-</body>
+		<br>
+		<div id="Copyright" class="center">
+			<h5>&copy; 2020, Amazon Web Services, Inc. or its Affiliates. All rights reserved.</h5>
+		</div>
+	</body>
 </html>
