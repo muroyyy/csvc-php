cbf1611e2a82fef28a4eb58e61a577e963c8428c
muroyyy <amirul.faiz01@gmail.com>
2025-08-15 17:00:49 +0800

    updated get-parameters to use the composer dependency, and also updated both of the queries files

--- get-parameters.php
+++ get-parameters.php
@@ -1,30 +1,33 @@
 <?php
   error_log('Retrieving settings');
-  require 'aws.phar';
+  require 'vendor/autoload.php'; // Use Composer autoload
+
+  use Aws\SecretsManager\SecretsManagerClient;
+  use Aws\Exception\AwsException;
 
   $az = file_get_contents('http://169.254.169.254/latest/meta-data/placement/availability-zone');
   $region = substr($az, 0, -1);
-  $ssm_client = new Aws\Ssm\SsmClient([
-     'version' => 'latest',
-     'region'  => $region
+
+  $secretsManagerClient = new SecretsManagerClient([
+    'version' => 'latest',
+    'region'  => $region
   ]);
 
   try {
-    // Retrieve the JSON parameter from Parameter Store
-    $result = $ssm_client->getParameter([
-      'Name' => '/test',
-      'WithDecryption' => true
+    // Retrieve the secret from Secrets Manager
+    $result = $secretsManagerClient->getSecretValue([
+      'SecretId' => 'milo-db-credentials'
     ]);
 
-    // Decode the JSON value
-    $json = json_decode($result['Parameter']['Value'], true);
+    // Decode the JSON secret string
+    $json = json_decode($result['SecretString'], true);
 
-    $ep = $json['rds_endpoint'];
-    $db = $json['db_name'];
+    $ep = $json['endpoint'];
+    $db = $json['database'];
     $un = $json['username'];
     $pw = $json['password'];
   }
-  catch (Exception $e) {
+  catch (AwsException $e) {
     $ep = '';
     $db = '';
     $un = '';
