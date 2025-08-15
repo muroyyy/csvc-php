c0672d797d42ff69d19a6a53597cb09dde53713f
muroyyy <amirul.faiz01@gmail.com>
2025-08-15 17:42:02 +0800

    Add detailed error logging to get-parameters.php for AWS Secrets Manager

--- get-parameters.php
+++ get-parameters.php
@@ -18,16 +18,32 @@
     $result = $secretsManagerClient->getSecretValue([
       'SecretId' => 'milo-db-credentials'
     ]);
+    error_log('Successfully retrieved secret from Secrets Manager.');
 
     // Decode the JSON secret string
     $json = json_decode($result['SecretString'], true);
+
+    if ($json === null) {
+      error_log('Failed to decode JSON from Secrets Manager: ' . json_last_error_msg());
+      throw new Exception('JSON decode error: ' . json_last_error_msg());
+    }
 
     $ep = $json['endpoint'];
     $db = $json['database'];
     $un = $json['username'];
     $pw = $json['password'];
+
+    error_log("Database credentials loaded: endpoint=$ep, database=$db, username=$un");
   }
   catch (AwsException $e) {
+    error_log('AWS Exception when retrieving secret: ' . $e->getAwsErrorMessage());
+    $ep = '';
+    $db = '';
+    $un = '';
+    $pw = '';
+  }
+  catch (Exception $e) {
+    error_log('General Exception: ' . $e->getMessage());
     $ep = '';
     $db = '';
     $un = '';
